f6e07c1a49b23d711654e73e4e188392c51d94f1
93n105 <ernestkwablah19@gmail.com>
2024-03-07 20:29:27 +0000

    Modify app.py to handle post requests

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    text = ""
+    if request.method == 'POST':
+        text = request.form.get('email-content')
+    return render_template('index.html', text=text)
 
 
 if __name__ == '__main__':
