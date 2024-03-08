29c7d5f31f8414ed16130014e23ad0de41c74380
93n105 <ernestkwablah19@gmail.com>
2024-03-08 13:44:38 +0000

    uploading models and updating app.py

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
+import pickle
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+cv = pickle.load(open("models/cv.pkl"))
+clf = pickle.load(open("models/clf.pkl"))
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/")
 def home():
-    text = ""
-    if request.method == 'POST':
-        text = request.form.get('email-content')
-    return render_template('index.html', text=text)
+    return render_template("index.html")
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    email = request.form.get('email-content')
+    tokenized_email = cv.transform([email])
+    prediction = clf.predict(tokenized_email)
+    prediction = 1 if prediction == 1 else -1
+    return render_template("index.html", prediction=prediction, email=email)
 
 
 if __name__ == '__main__':
